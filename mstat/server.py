565d93a7d798025f0806d36f1f8c2707c543ffd4
Dmitry Korotkih <ds@commfactor.ru>
2016-11-09 16:53:41 +0300

    orig control added

--- mstat/server.py
+++ mstat/server.py
@@ -33,6 +33,7 @@ class ApiServer(BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             self.wfile.write(bytes(resp, "utf8"))
 
